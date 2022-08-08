7fd139552c6a6f50f6c54c26a7da88481180eade
AnLaxina <anilovlaxina@gmail.com>
2022-08-07 20:23:23 -0700

    Added comments to the added libraries

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+# Importing the tkinter library
 from tkinter import *
+
+# Importing the pytube library
+# I import the YouTube class because I am trying to download videos.
+# pytube also offers to import other classes such as Channel and Playlist.
+# However, they are not needed in this case.
+from pytube import YouTube
 
 main = Tk()
 
